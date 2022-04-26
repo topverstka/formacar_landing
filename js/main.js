41e3cf7976ff6ea8a572da98af635d6f39c9f01a
DanilaMasterPro <daniilbortvin@gmail.com>
2022-04-26 23:17:20 +0300

    pixel perfect

--- js/main.js
+++ js/main.js
@@ -60,6 +60,29 @@ function faq(){
 
 faq();
 
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.querySelector(".header");
+
+// Get the offset position of the navbar
+var sticky = 1000;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
 // Валидация формы
 function validationForm() {
     const name = find('#user_name')
